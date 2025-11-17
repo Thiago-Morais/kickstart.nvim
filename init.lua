09c0b15dd058320f33de42c3952f65d92c4e478f
Thiago Morais <thiolimor@gmail.com>
2025-11-17 01:16:36 -0300

    fix: Navigate intellisense with tab and confirm with enter

--- init.lua
+++ init.lua
@@ -733,7 +733,9 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'super-tab',
+        preset = 'enter',
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<Tab>'] = { 'select_next', 'fallback' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
