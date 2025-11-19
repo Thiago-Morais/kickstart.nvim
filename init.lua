dc0e8829d2d8dec94ede7896feb128f3ccd926b6
Thiago Morais <thiolimor@gmail.com>
2025-11-19 02:20:42 -0300

    feat: Color Diffview like github
    The original diffview uses the same color on both the left and right sides
    
    With the example taken from [this comment](https://github.com/sindrets/diffview.nvim/pull/258#issuecomment-1408689220) we could use custom colors for the left and right sides separately

--- init.lua
+++ init.lua
@@ -981,7 +981,29 @@ require('lazy').setup({
     },
   },
   { 'tpope/vim-fugitive' },
-  { 'sindrets/diffview.nvim' },
+  {
+    'sindrets/diffview.nvim',
+    opts = {
+      hooks = {
+        -- Taken from this example: https://github.com/sindrets/diffview.nvim/pull/258#issuecomment-1408689220
+        diff_buf_win_enter = function(bufnr, winid, ctx)
+          if ctx.layout_name:match '^diff2' then
+            if ctx.symbol == 'a' then
+              vim.opt_local.winhl = table.concat({
+                'DiffText:DiffviewDiffAddAsDelete',
+                'DiffChange:DiffviewLineChangeDiffDelete',
+              }, ',')
+            elseif ctx.symbol == 'b' then
+              vim.opt_local.winhl = table.concat({
+                'DiffText:DiffviewDiffAdd',
+                'DiffChange:DiffviewLineChangeDiffAdd',
+              }, ',')
+            end
+          end
+        end,
+      },
+    },
+  },
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
