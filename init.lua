715d393157b32508a4a612dec121fe168613a864
Cyber Falcon <thiolimor@gmail.com>
2025-11-29 09:34:38 -0300

    feat: Add lorem ipsum

--- init.lua
+++ init.lua
@@ -1019,6 +1019,17 @@ require('lazy').setup({
     cmd = { 'LiveServerStart', 'LiveServerStop' },
     config = true,
   },
+  {
+    'derektata/lorem.nvim',
+    config = function()
+      require('lorem').opts {
+        sentence_length = 'mixed', -- using a default configuration
+        comma_chance = 0.3, -- 30% chance to insert a comma
+        max_commas = 2, -- maximum 2 commas per sentence
+        debounce_ms = 200, -- default debounce time in milliseconds
+      }
+    end,
+  },
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
