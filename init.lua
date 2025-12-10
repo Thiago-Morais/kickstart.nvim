0a6b2321c69d1adcd7033164308683a0af6c2791
Cyber Falcon <thiolimor@gmail.com>
2025-12-10 00:14:26 -0300

    feat: Add mini.align plugin

--- init.lua
+++ init.lua
@@ -869,7 +869,9 @@ require('lazy').setup({
         return '%2l:%-2v'
       end
 
-      require('mini.move').setup {}
+      require('mini.align').setup()
+      require('mini.move').setup()
+      -- require('mini.bracketed').setup()
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
