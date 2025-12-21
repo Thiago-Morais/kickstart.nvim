0d9d6811b54897eac9595a24bdd41d09a5537be6
Cyber Falcon <thiolimor@gmail.com>
2025-12-21 07:35:51 -0300

    feat: Add neotest

---
+++ lua/custom/plugins/neotest.lua
@@ -0,0 +1,21 @@
+return {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  opts = {
+    adapters = {
+      require 'neotest-python' {
+        dap = { justMyCode = false },
+      },
+      require 'neotest-plenary',
+      require 'neotest-vim-test' {
+        ignore_file_types = { 'python', 'vim', 'lua' },
+      },
+      require 'neotest-jest',
+    },
+  },
+}
