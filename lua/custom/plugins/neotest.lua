613ed68dd60f73a6501d64c0256633b2b493ec3e
Cyber Falcon <thiolimor@gmail.com>
2025-12-22 12:24:38 -0300

    fix: Load neotest after dependencies

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -5,17 +5,25 @@ return {
     'nvim-lua/plenary.nvim',
     'antoinemadec/FixCursorHold.nvim',
     'nvim-treesitter/nvim-treesitter',
+    -- Adapters
+    'nvim-neotest/neotest-python',
+    'nvim-neotest/neotest-plenary',
+    'nvim-neotest/neotest-vim-test',
+    'nvim-neotest/neotest-jest',
   },
-  opts = {
-    adapters = {
-      require 'neotest-python' {
-        dap = { justMyCode = false },
+
+  config = function()
+    require('neotest').setup {
+      adapters = {
+        require 'neotest-python' {
+          dap = { justMyCode = false },
+        },
+        require 'neotest-plenary',
+        require 'neotest-vim-test' {
+          ignore_file_types = { 'python', 'vim', 'lua' },
+        },
+        require 'neotest-jest',
       },
-      require 'neotest-plenary',
-      require 'neotest-vim-test' {
-        ignore_file_types = { 'python', 'vim', 'lua' },
-      },
-      require 'neotest-jest',
-    },
-  },
+    }
+
 }
