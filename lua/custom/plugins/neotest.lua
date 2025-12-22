11fd78e5cc530eaef4e9f454f57f318c5d06d1f2
Cyber Falcon <thiolimor@gmail.com>
2025-12-22 12:25:19 -0300

    feat: Add neotest keymaps

--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -26,4 +26,13 @@ return {
       },
     }
 
+    vim.keymap.set('n', '<leader>tn', '<cmd>Neotest run<CR>', { desc = 'Run the [N]earest test' })
+    vim.keymap.set('n', '<leader>tf', '<cmd>Neotest run file<CR>', { desc = 'Run the current [F]ile' })
+    vim.keymap.set('n', '<leader>tl', '<cmd>Neotest run last<CR>', { desc = 'Run [L]ast file' })
+    vim.keymap.set('n', '<leader>tN', '<cmd>Neotest stop<CR>', { desc = 'Stop the [N]earest test' })
+    vim.keymap.set('n', '<leader>ta', '<cmd>Neotest attach<CR>', { desc = '[A]ttach to the nearest test' })
+    vim.keymap.set('n', '<leader>ts', '<cmd>Neotest summary<CR>', { desc = 'Open [S]ummary panel' })
+    vim.keymap.set('n', '<leader>to', '<cmd>Neotest output-panel<CR>', { desc = 'Open [O]utpup panel' })
+    vim.keymap.set('n', '<leader>td', '<cmd>lua require("neotest").run.run ({ strategy = "dap" })<CR>', { desc = '[D]ebug the nearest test' })
+  end,
 }
