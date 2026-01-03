2ad96daba4c21a43f4fd6a5e9f825c48e49e028f
Cyber Falcon <thiolimor@gmail.com>
2026-01-02 22:55:21 -0300

    fix: Search hidden files with `live_grep`

--- init.lua
+++ init.lua
@@ -366,9 +366,11 @@ require('lazy').setup({
       end, { desc = '[S]earch hidden [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sg', function()
+        builtin.live_grep { additional_args = { '--hidden', '--ignore-vcs' } }
+      end, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sG', function()
-        builtin.live_grep { additional_args = { '--no-ignore-vcs' } }
+        builtin.live_grep { additional_args = { '--hidden', '--no-ignore-vcs' } }
       end, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
