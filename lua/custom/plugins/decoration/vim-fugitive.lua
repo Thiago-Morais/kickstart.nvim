b455edf7fefaf718803d376c4b6488e317057569
Cyber Falcon <thiolimor@gmail.com>
2026-01-06 17:15:31 -0300

    feat(fugitive): Add keymaps to fugitive
    Also use event `VeryLazy` so that we can load help_tags before using the
    keymap
    
    Without this event call, when we use the `keys` flag we can't do a
    `:h fugitive` until we run one of the keymaps e.g. `<leader>gb`
    
    Using this event flag we load the help_tags wihtout need to do the
    keymap command

--- lua/custom/plugins/decoration/vim-fugitive.lua
+++ lua/custom/plugins/decoration/vim-fugitive.lua
@@ -1,1 +1,8 @@
-return { 'tpope/vim-fugitive' }
+return {
+  'tpope/vim-fugitive',
+  event = 'VeryLazy',
+  keys = {
+    { '<leader>gb', '<cmd>Git blame<CR>', desc = '[G]it [B]lame' },
+    { '<leader>gs', '<cmd>Git<CR>', desc = '[G]it [S]tatus' },
+  },
+}
