e8b917a614f49c5dc1072a606bf6d07836b84f48
Cyber Falcon <thiolimor@gmail.com>
2026-01-06 16:50:06 -0300

    fix(treesitter): Remove the main module override
    A new update made so that we don't need to specify a main module anymore
    (?)

--- lua/custom/plugins/decoration/nvim-treesitter.lua
+++ lua/custom/plugins/decoration/nvim-treesitter.lua
@@ -1,7 +1,6 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'json', 'jsonc' },
@@ -23,4 +22,4 @@ return { -- Highlight, edit, and navigate code
   --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
   --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
   --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  }
+}
