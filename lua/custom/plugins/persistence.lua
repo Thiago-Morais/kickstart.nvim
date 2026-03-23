f7dc499264b39c35e32b62e86b618a682bb9bce0
Cyber Falcon <thiolimor@gmail.com>
2026-03-23 13:38:45 -0300

    feat(persistence): Add keymaps

--- lua/custom/plugins/persistence.lua
+++ lua/custom/plugins/persistence.lua
@@ -8,4 +8,34 @@ return { -- Lua
     need = 1,
     branch = true, -- use git branch to save session
   },
+  keys = {
+    {
+      '<leader>ps',
+      function()
+        require('persistence').load()
+      end,
+      desc = 'Load the [S]ession for the current directory',
+    },
+    {
+      '<leader>pS',
+      function()
+        require('persistence').select()
+      end,
+      desc = '[S]elect a [S]ession to load',
+    },
+    {
+      '<leader>pl',
+      function()
+        require('persistence').load { last = true }
+      end,
+      desc = 'Load the [L]ast session',
+    },
+    {
+      '<leader>pd',
+      function()
+        require('persistence').stop()
+      end,
+      desc = "stop/[D]rop Persistence => session won't be saved on exit",
+    },
+  },
 }
