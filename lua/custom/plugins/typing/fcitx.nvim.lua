e1b3b12de35c01cf03d770d65acc03c69a799418
Cyber Falcon <thiolimor@gmail.com>
2026-07-13 18:46:46 -0300

    feat: Add fcitx config to neovim

---
+++ lua/custom/plugins/typing/fcitx.nvim.lua
@@ -0,0 +1,11 @@
+return {
+  enabled = false,
+  'junyixu/fcitx.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('fcitx').setup {
+      -- enable_cmdline = true, -- also toggle when entering / or ?
+    }
+  end,
+}
