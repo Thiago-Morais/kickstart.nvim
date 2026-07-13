7235bda42ceb257b1aa8b2a2a340030b6b2c739a
Cyber Falcon <thiolimor@gmail.com>
2026-07-13 18:59:19 -0300

    feat: Enable Line Blame by default

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -9,6 +9,8 @@ return {
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
+        gitsigns.toggle_current_line_blame(true)
+
         local function map(mode, l, r, opts)
           opts = opts or {}
           opts.buffer = bufnr
